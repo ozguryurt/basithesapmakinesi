92b8bb1ddb80778c474aaa79e6a21d0e73a4335a
ozguryurt <93544343+ozguryurt@users.noreply.github.com>
2021-11-05 23:52:43 +0300

    Add files via upload

---
+++ hesap makinesi/HesapMakinesi/HesapMakinesi/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HesapMakinesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[>] 1. SAYI:");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("[>] İŞLEM SEÇİNİZ: (+) (-) (*) (/)");
+            string islem = Console.ReadLine();
+
+            Console.WriteLine("[>] 2. SAYI:");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            if(islem == "+")
+            {
+                Console.WriteLine(sayi1 + " + " + sayi2 + " = " + (sayi1+sayi2));
+            }
+            else if (islem == "-")
+            {
+                Console.WriteLine(sayi1 + " - " + sayi2 + " = " + (sayi1 - sayi2));
+            }
+            else if (islem == "*")
+            {
+                Console.WriteLine(sayi1 + " * " + sayi2 + " = " + (sayi1 * sayi2));
+            }
+            else if (islem == "/")
+            {
+                if(sayi2 != 0)
+                {
+                    Console.WriteLine(sayi1 + " / " + sayi2 + " = " + ((double)sayi1 / (double)sayi2));
+                }
+                else if (sayi2 == 0)
+                {
+                    Console.WriteLine(sayi1 + " / 0 =  Tanımsız");
+                }
+            }
+            else if(islem != "+" && islem != "-" && islem != "*" && islem != "/")
+            {
+                Console.WriteLine("[>] Geçersiz işlem girdiğiniz için işlem yapılamadı.");
+            }
+        }
+    }
+}
